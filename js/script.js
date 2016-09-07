0089438edc0418a26e3b230dd6ef85ecba3ad64f
AtillaSzikora <szikora.atilla@gmail.com>
2016-09-07 22:53:30 +0200

    Full url validation added.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-// alert($('urlInput').id);
+var urlError = '<div class="form-control-feedback">URL has to start with http:// and end with .xml!</div>';
+
+$("#urlInput").on('blur', urlValidator);
+
+function urlValidator() {
+    if (!($(this).val().startsWith("http://") && $(this).val().endsWith(".xml"))) {
+        $(this).parent().addClass("has-danger");
+        $(this).addClass("form-control-danger");
+        if(!$('.form-control-feedback').length) {
+            $(urlError).insertAfter($(this));
+        }
+    } else {
+        $('.form-control-feedback').remove();
+        $(this).removeClass("form-control-danger");
+        $(this).addClass("form-control-success");
+        $(this).parent().removeClass("has-danger");
+        $(this).parent().addClass("has-success");
+    }
+}
 
