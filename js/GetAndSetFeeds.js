a0cdb04603d9ad32ef5b609d924159a06bae7cff
AtillaSzikora <szikora.atilla@gmail.com>
2016-09-11 20:07:17 +0200

    Delete feed done.

--- js/GetAndSetFeeds.js
+++ js/GetAndSetFeeds.js
@@ -1,5 +1,4 @@
-
-$newInputRow = `<div class="row">
+var $newInputRow = `<div class="row">
                     <div class="form-group col-md-3">
                         <input type="text" class="form-control">
                     </div>
@@ -10,13 +9,24 @@ $newInputRow = `<div class="row">
                         <input type="number" class="form-control" min="1">
                     </div>
                     <div class="form-group col-md-3">
-                        <button class="btn btn-outline-primary"><i class="fa fa-pencil"></i></button>
-                        <button class="btn btn-outline-primary"><i class="fa fa-trash"></i></button>
-                        <button class="btn btn-outline-primary"><i class="fa fa-star"></i></button>
+                        <button class="btn btn-outline-warning"><i class="fa fa-pencil"></i></button>
+                        <button class="btn btn-outline-danger"><i class="fa fa-trash"></i></button>
+                        <button class="btn btn-outline-success"><i class="fa fa-star"></i></button>
                     </div>
                 </div>`;
 
 listStoredFeeds();
+$('.btn-outline-danger').on('click', deleteFeed);
+
+function listStoredFeeds() {
+    $('#allFeeds').empty();
+    $.each(localStorage, function (key, value) {
+        $('#allFeeds').append($newInputRow);
+        $('.row:last').children()[0].getElementsByTagName('input')[0].value = JSON.parse(value).name;
+        $('.row:last').children()[1].getElementsByTagName('input')[0].value = JSON.parse(value).url;
+        $('.row:last').children()[2].getElementsByTagName('input')[0].value = JSON.parse(value).counter;
+    });
+}
 
 function addNewFeedToStorage() {
     var feedObject = {
@@ -34,12 +44,8 @@ function addNewFeedToStorage() {
     }
 }
 
-function listStoredFeeds() {
-    $('#allFeeds').empty();
-    $.each(localStorage, function (key, value) {
-        $('#allFeeds').append($newInputRow);
-        $('.row:last').children()[0].getElementsByTagName('input')[0].value = JSON.parse(value).name;
-        $('.row:last').children()[1].getElementsByTagName('input')[0].value = JSON.parse(value).url;
-        $('.row:last').children()[2].getElementsByTagName('input')[0].value = JSON.parse(value).counter;
-    });
+function deleteFeed() {
+    var $chosenFeed = $(this).parent().siblings()[0].getElementsByTagName('input')[0].value;
+    $($(this).parent().parent()).remove();
+    localStorage.removeItem($chosenFeed);
 }
